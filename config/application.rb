3c9ff7cbd2cf7c3246e49808ea4005f74b6a8308
Ho Si Binh <e0411175@u.nus.edu>
2020-06-18 09:18:27 +0800

    Configure autoloading of libraries in /lib

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module SleepLoggerApi
   class Application < Rails::Application
+    # Loads libraries in lib.
+    config.autoload_paths << Rails.root.join('lib')
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
