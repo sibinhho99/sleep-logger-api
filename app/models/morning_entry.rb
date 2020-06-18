3795fcf4b31b6645f1e929de02514642ce770dbc
Ho Si Binh <e0411175@u.nus.edu>
2020-06-18 08:52:37 +0800

    Add morning entry validation

--- app/models/morning_entry.rb
+++ app/models/morning_entry.rb
@@ -1,2 +1,7 @@
 class MorningEntry < ApplicationRecord
+  validates :bedtime, presence: true
+  validates :wake_up_time, presence: true
+  validates :ease_of_sleep, inclusion: 0..3, presence: true
+  validates :hours_of_sleep, presence: true
+  validates :morning_entry, presence: true
 end
