f9359f4c6f3960a1184881fc6dc34babca4979bd
Ho Si Binh <e0411175@u.nus.edu>
2020-06-19 01:11:55 +0800

    Add association

--- app/models/morning_entry.rb
+++ app/models/morning_entry.rb
@@ -1,4 +1,5 @@
 class MorningEntry < ApplicationRecord
+  belongs_to :user
   validates :bedtime, presence: true
   validates :wake_up_time, presence: true
   validates :ease_of_sleep, inclusion: 0..3, presence: true
