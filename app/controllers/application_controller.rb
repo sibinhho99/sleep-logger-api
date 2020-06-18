9442c6d808fe0351131599fa770a23d85565a08c
Ho Si Binh <e0411175@u.nus.edu>
2020-06-18 22:08:33 +0800

    Add user CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     #Overrides CSRF protection
     skip_before_action :verify_authenticity_token
-    # before_action :authenticate_request
+    before_action :authenticate_request
     attr_reader :current_user
 
     private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,13 @@
 class UsersController < ApplicationController
-  def index
-    @users = MorningEntry.all
-    render json: @users, status: :ok
-  end
+  skip_before_action :authenticate_request, only: [:create]
 
   def create
-    @user = MorningEntry.new(user_params)
+    @user = User.new(user_params)
     @user.save
     render json: @user, status: :created
   end
 
-  def show
-    @user = MorningEntry.find(params[:id])
-    if @user
-      render json: @user, status: :ok
-    else
-      render status: :unprocessable_entity
-    end
-  end
-
-  def update
-    @user = MorningEntry.find(params[:id])
-    if @user.update(user_params)
-      render json: @user, status: :ok
-    else
-      render status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @user = MorningEntry.find(params[:id])
-    if @user.destroy
-      render status: :ok
-    else
-      render status: :unprocessable_entity
-    end
-  end
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
