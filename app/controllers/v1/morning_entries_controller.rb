9b507b0d3710837aa4fde4e4c129019c36f0cdf7
Ho Si Binh <e0411175@u.nus.edu>
2020-06-19 17:34:03 +0800

    Readd morning entry

--- app/controllers/v1/morning_entries_controller.rb
+++ app/controllers/v1/morning_entries_controller.rb
@@ -1,43 +1,42 @@
 class V1::MorningEntriesController < ApplicationController
-    def index
-      @morning_entries = @current_user.morning_entries
-      render json: @morning_entries, status: :ok
-    end
-    
-    def create
-      @morning_entry = @current_user.morning_entries.build(morning_entry_params)
-      render json: @morning_entry, status: :created
-    end
-  
-    def show
-      @morning_entry = MorningEntry.find(params[:id])
-      if @morning_entry
-        render json: @morning_entry, status: :ok
-      else
-        render status: :unprocessable_entity
-      end
-    end
-  
-    def update
-      @morning_entry = MorningEntry.find(params[:id])
-      if @morning_entry.update(morning_entry_params)
-        render json: @morning_entry, status: :ok
-      else
-        render status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      @morning_entry = MorningEntry.find(params[:id])
-      if @morning_entry.destroy
-        render status: :ok
-      else
-        render status: :unprocessable_entity
-      end
-    end
-  
-    def morning_entry_params
-        params.require(:morning_entry).permit(:bed_time, :wake_up_time, :ease_of_sleep, :hours_of_sleep, :morning_feeling)
+  def index
+    @morning_entries = @current_user.morning_entries
+    render json: @morning_entries, status: :ok
+  end
+
+  def create
+    @morning_entry = @current_user.morning_entries.create!(morning_entry_params)
+    render json: @morning_entry, status: :created
+  end
+
+  def show
+    @morning_entry = MorningEntry.find(params[:id])
+    if @morning_entry
+      render json: @morning_entry, status: :ok
+    else
+      render status: :unprocessable_entity
     end
   end
-  
+
+  def update
+    @morning_entry = MorningEntry.find(params[:id])
+    if @morning_entry.update(morning_entry_params)
+      render json: @morning_entry, status: :ok
+    else
+      render status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @morning_entry = MorningEntry.find(params[:id])
+    if @morning_entry.destroy
+      render status: :ok
+    else
+      render status: :unprocessable_entity
+    end
+  end
+
+  def morning_entry_params
+    params.require(:morning_entry).permit(:bed_time, :wake_up_time, :ease_of_sleep, :hours_of_sleep, :morning_feeling)
+  end
+end
--- db/migrate/20200619095839_create_morning_entries.rb
+++ db/migrate/20200619095839_create_morning_entries.rb
@@ -6,6 +6,8 @@ class CreateMorningEntries < ActiveRecord::Migration[6.0]
       t.integer :ease_of_sleep
       t.integer :hours_of_sleep
       t.integer :morning_feeling
+      t.references :user, null: false, foreign_key: true
+
       t.timestamps
     end
   end
