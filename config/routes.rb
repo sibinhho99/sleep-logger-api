e9dc2f399d0d575f8f17c46ebd9beb5575362c34
Ho Si Binh <e0411175@u.nus.edu>
2020-07-17 21:48:20 +0800

    Add getting user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
   get 'get_tips', to: 'get_tips#get_tips'
+  get 'get_user', to: 'users#get_user'
 
   resources :users
   namespace :v1 do
